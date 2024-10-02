bb2341d62d22b1e113d05c9009f16ab00b3babbf
Kaelan Smith <kaelancsmith@gmail.com>
2024-10-02 16:35:17 -0700

    enable parsing support for Synced Patterns, fix parsing of "anchor" attribute

--- src/Transformers/CoreBlockTransformer.php
+++ src/Transformers/CoreBlockTransformer.php
@@ -11,32 +11,55 @@ class CoreBlockTransformer extends AbstractBlockTransformer
 
   public function transform(WP_Block $block, int|null $postId = null): array
   {
-    $attrs = $block->attributes;
-    $this->parseAttributes($block, $attrs, $postId);
+    $attrs = $this->parseAttributes($block, $postId);
 
     $formattedBlock = $this->formatBaseBlock($block, $attrs);
 
-    if ($this->shouldIncludeRendered($formattedBlock)) {
+    if ($formattedBlock['name'] == 'core/block' && isset($attrs['ref'])) {
+      /** === Synced Patterns ===
+       * A synced pattern will only have a 'ref' attribute referencing its post ID. So
+       * we need to parse that separate post's blocks and return the result. BlockParser's
+       * transformBlocks method will handle flattening the nested array of blocks.
+       */
+      $formattedBlock = $this->parser->parseBlocksFromPost($attrs['ref']);
+    } else if ($this->shouldIncludeRendered($formattedBlock)) {
       $formattedBlock['rendered'] = do_shortcode($block->render());
     }
 
     return $formattedBlock;
   }
 
-  protected function parseAttributes(WP_Block $block, array &$attrs, int $postId): void
+  protected function parseAttributes(WP_Block $block, int $postId): array
   {
+    $blockAttrs = $block->attributes;
+    $blockTypeAttrs = $block->block_type->attributes;
+    $supports = $block->block_type->supports;
+
+    // Manually add anchor attribute if supported:
+    if ($supports && isset($supports['anchor']) && $supports['anchor']) {
+      $blockTypeAttrs['anchor'] = [
+        'type' => 'string',
+        'default' => '',
+        'source' => 'attribute',
+        'attribute' => 'id',
+        'selector' => '*'
+      ];
+    }
+
     $attributeParser = new AttributeParser();
 
-    foreach ($block->block_type->attributes as $key => $attribute) {
-      if (!isset($attrs[$key]) || $attrs[$key] == "") {
+    foreach ($blockTypeAttrs as $key => $attribute) {
+      if (!isset($blockAttrs[$key]) || $blockAttrs[$key] == "") {
         $attrValue = $attributeParser->getAttribute($attribute, $block->inner_html, $postId);
         if ($attrValue !== null) {
-          $attrs[$key] = $attrValue;
+          $blockAttrs[$key] = $attrValue;
         }
       }
     }
 
-    $this->removeUnwantedAttributes($attrs);
+    $this->removeUnwantedAttributes($blockAttrs);
+
+    return $blockAttrs;
   }
 
   protected function shouldIncludeRendered(array $formattedBlock): bool
